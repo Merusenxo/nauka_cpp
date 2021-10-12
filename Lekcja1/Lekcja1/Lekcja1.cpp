81f60effd1fb143d4f8077e68eb9b7cd9dc55200
M.Ulaski <mrrufus111@gmail.com>
2021-10-12 22:56:04 +0200

    max3,4,5 zrobione

--- Lekcja1/Lekcja1/Lekcja1.cpp
+++ Lekcja1/Lekcja1/Lekcja1.cpp
@@ -1,20 +1,75 @@
-﻿// Lekcja1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+int a, b, c, d, e;
+
+void getValues() 
+{
+    cout << "proszę podaj pierwszą liczbe:";
+    cin >> a;
+    cout << "proszę podaj drugą liczbę:";
+    cin >> b;
+    cout << "proszę podaj trzecią liczbę:";
+    cin >> c;
+}
+int getMax()
+{
+    int max=INT_MIN;
+
+    if ((a > b) && (a > c))
+    {
+        max = a;
+    }
+    else if ((b > a) && (b > c))
+    {
+        max = b;
+    }
+    else if ((c > b) && (c > a))
+    {
+        max = c;
+    }
+    return max; 
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "");
+    int max=0;
+ 
+    getValues(); //pobieram wartości od użytkownika 
+    max=getMax(); //Wyświetlam użytkownikowi największą liczbę
+
+    cout << "największa jest liczba " << max << "" << endl;
+  
+    //max4 teraz
+
+
+    getValues();
+    
+    cout << "proszę podaj czwartą liczbę:";
+    cin >> d;
+
+    max=getMax();
+
+    max = (d > max) ? d : max;
+
+    cout << "największa jest liczba: " << max << "" << endl;
+     
+    // max 5 
+
+
+    getValues();
+    cout << "proszę podaj czwartą liczbę:";
+    cin >> d;
+    cout << "proszę podaj piątą liczbę:";
+    cin >> e;
+    
+    max = getMax();
+    max = ((d > max) && (d > e)) ? d : ((e > max) && (e > d)) ? e : max;
+
+    
+    cout << "największa jest liczba: " << max << "" << endl;
+
+    return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
